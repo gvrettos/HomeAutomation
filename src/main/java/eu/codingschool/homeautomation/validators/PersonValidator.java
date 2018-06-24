19bbb56f3d92c7b5857b85650de319ea603faad8
George Vrettos <lukedevgr@gmail.com>
2018-06-24 18:21:12 +0300

    Refactor code related to Person functionality

--- src/main/java/eu/codingschool/homeautomation/validators/PersonValidator.java
+++ src/main/java/eu/codingschool/homeautomation/validators/PersonValidator.java
@@ -7,36 +7,31 @@ import org.springframework.validation.Validator;
 
 import eu.codingschool.homeautomation.model.Person;
 
+@Component
+public class PersonValidator implements Validator {
 
-
-	@Component
-	public class PersonValidator implements Validator {
-
-	    @Override
-	    public boolean supports(Class<?> aClass) {
-	        return Person.class.equals(aClass);
-	    }
-
-	    @Override
-	    public void validate(Object o, Errors errors) {
-	        Person person = (Person) o;
-
-	        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "NotEmpty");
-	        if (person.getName().length() < 3 || person.getName().length() > 32) {
-	            errors.rejectValue("name", "Size");
-	        }
-	        
-	        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "surname", "NotEmpty");
-	        if (person.getSurname().length() < 3 || person.getSurname().length() > 32) {
-	            errors.rejectValue("surname", "Size");
-	        }
-
-	        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "Wrong email format");
-	        if (!person.getEmail().contains("@") ) {
-	            errors.rejectValue("email", "Size");
-	        }
-	    }
+	@Override
+	public boolean supports(Class<?> aClass) {
+		return Person.class.equals(aClass);
 	}
-	
-	
-	
+
+	@Override
+	public void validate(Object o, Errors errors) {
+		Person person = (Person) o;
+
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "NotEmpty");
+		if (person.getName().length() < 3 || person.getName().length() > 32) {
+			errors.rejectValue("name", "Size");
+		}
+
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "surname", "NotEmpty");
+		if (person.getSurname().length() < 3 || person.getSurname().length() > 32) {
+			errors.rejectValue("surname", "Size");
+		}
+
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "Wrong email format");
+		if (!person.getEmail().contains("@")) {
+			errors.rejectValue("email", "Size");
+		}
+	}
+}
