3ff57ff2ae5a723e0d9a0812376b9fa58b6c860b
George Vrettos <lukedevgr@gmail.com>
2020-08-04 23:21:48 +0300

    Add tests for repositories

---
+++ src/test/java/eu/codingschool/homeautomation/repositories/DeviceTypeRepositoryTest.java
@@ -0,0 +1,17 @@
+package eu.codingschool.homeautomation.repositories;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class DeviceTypeRepositoryTest {
+	
+	@Test
+	public void pass() {
+		// No tests needed because no special repository methods have been defined
+	}
+	
+}
